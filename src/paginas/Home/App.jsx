255b72d8cef19857042bb24d0b118c55a3605265
Yan Strike <yyupanquij@gmail.com>
2025-08-11 23:53:22 -0500

    react update

---
+++ src/paginas/Home/App.jsx
@@ -0,0 +1,18 @@
+import { Footer } from "./components/Footer";
+import { Header } from "./components/Header";
+import { Main } from "./components/Main";
+
+
+function App() {
+// codigo js entre antes del return
+var contador = 0;
+  return (
+    <>
+    <Header /><br/>
+    <Main /><br/>
+    <Footer />
+    </>
+  )
+}
+
+export default App
