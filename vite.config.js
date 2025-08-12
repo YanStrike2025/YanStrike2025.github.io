be9e661da1ee5059cdf440ddfe5121252850195b
YanStrike2025 <yyupanquij@gmail.com>
2025-08-12 16:57:23 -0500

    react udpate live

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/YanStrike2025.github.io/' // change if your repo name is different
 })
