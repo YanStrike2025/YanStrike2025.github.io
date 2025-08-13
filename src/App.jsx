d3a5e88e28514ffd285dff845ec4a91a6beec3b2
Yan Strike <yyupanquij@gmail.com>
2025-08-12 23:19:47 -0500

    finally a react prjct

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,16 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Footer } from "./paginas/Home/components/Footer";
+import { Header } from "./paginas/Home/components/Header";
+import { Main } from "./paginas/Home/components/Main";
+
 
 function App() {
-  const [count, setCount] = useState(0)
-
+// codigo js entre antes del return
+var contador = 0;
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <Header /><br/>
+    <Main /><br/>
+    <Footer />
     </>
   )
 }
