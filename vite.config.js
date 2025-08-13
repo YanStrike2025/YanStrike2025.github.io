bf6dbb68fd2f543ac16703a1f22f1b86cd41e2e0
Yan Strike <yyupanquij@gmail.com>
2025-08-12 21:39:16 -0500

    final update

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/YanStrike2025.github.io/' // change if your repo name is different
+  base: '/', // 👈 importante
 })
